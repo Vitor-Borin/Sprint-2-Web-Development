507eebf98dfcb97a15b56eff3b466dcd901b89b6
Vitor Borin <vitor.borin06@gmail.com>
2026-05-15 18:18:19 -0300

    feat: adiciona slideshow automatico de imagens

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,21 @@
 const nomeSite = "MOBIX";
 const versao = "1.0";
+
+const imagens = [
+    "src/assets/img1.png",
+    "src/assets/img1.png",
+    "src/assets/img1.png"
+];
+
+let indiceImagem = 0;
+const imgSlide = document.querySelector(".hero-image img");
+
+function trocarImagem() {
+    indiceImagem++;
+    if (indiceImagem >= imagens.length) {
+        indiceImagem = 0;
+    }
+    imgSlide.src = imagens[indiceImagem];
+}
+
+setInterval(trocarImagem, 3000);
